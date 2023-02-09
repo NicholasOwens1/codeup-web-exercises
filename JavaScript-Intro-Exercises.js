9527c14e064e7f06acd1f5b90aeedd71650076f2
Nicholas Owens <nicholas.s.owens01@gmail.com>
2023-02-09 14:54:24 -0600

    adding operators-exercises

---
+++ JavaScript-Intro-Exercises.js
@@ -0,0 +1,172 @@
+// let a = 1;
+// let b = a++;
+// let c = ++a;
+//
+// a = 1;
+// b = 2;
+// c = 3;
+//
+// let d = "hello";
+// let e = false;
+//
+// d++;
+// e++;
+// d = NaN
+// e = 1
+
+
+// let perplexed;
+// perplexed + 2;
+// perplexed + 2 = NaN;
+
+// var price = 2.7;
+// price.toFixed(2);
+// price.toFixed = 2.70;
+
+// let price = "2.7";
+// price.toFixed(2);
+// error
+
+
+// isNan(0);
+// false
+//
+// isNan(1);
+// false
+//
+// isNaN("");
+// console.log(isNaN(""))
+// true
+//
+// isNaN("string");
+// true
+//
+// isNaN("0");
+// false
+//
+// isNaN("1");
+// false
+// console.log(isNaN("3.145"));
+// false
+// console.log(isNaN(Number.MAX_VALUE));
+// false
+// console.log(isNaN(Infinity));
+// false
+// console.log(isNaN(true))
+// false
+// console.log(isNaN("true"));
+// true
+// console.log(isNaN("false"));
+// console.log(isNaN(false));
+// false
+
+// console.log(!true);
+// false
+
+// console.log(!false);
+// true
+
+// console.log(!!true);
+// true
+
+// console.log(!!false);
+// false
+
+// console.log(!!0);
+// false
+
+// console.log(!!-0);
+// false
+
+// console.log(!!1);
+// true
+
+// console.log(!!-1);
+// true
+
+// console.log(!!0.1);
+// true
+
+// console.log(!!"hello");
+// true
+
+// console.log(!!"");
+// false
+
+// console.log(!!'');
+// false;
+
+// console.log(!!"false");
+// true;
+
+// console.log(!!"0");
+// false
+
+// !true
+// false
+//
+// !false
+// true
+//
+// !!true
+// true
+//
+// !!false
+// false
+
+// console.log(!!0)
+// false
+
+// console.log(!!-0)
+// false
+
+// console.log(!!1)
+// true
+
+// console.log(!!-1)
+// true
+
+// console.log(!!0.1)
+//true
+
+// console.log(!!"hello")
+// true
+
+// console.log(!!"")
+// true
+
+
+// console.log(!!'')
+// true
+
+// console.log(!!"false")
+// true
+
+// console.log(!!"0")
+// true
+
+let sample = "Hello Codeup";
+
+console.log(sample.length);
+
+console.log(sample.toLowerCase());
+
+console.log(sample.indexOf("c"));
+
+console.log(sample.indexOf("C"));
+
+console.log(sample.indexOf(" "));
+
+console.log(sample.substring(6));
+
+
+costPerday = 3
+let mermaid = costPerday * 3;
+let bear = costPerday * 5;
+let hercules = costPerday * 1;
+console.log( mermaid + bear + hercules);
+
+
+
+
+
