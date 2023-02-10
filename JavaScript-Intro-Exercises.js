57682861f1bc9858ec1fcfc7db9a32db8b4d3ac7
Nicholas Owens <nicholas.s.owens01@gmail.com>
2023-02-10 10:31:16 -0600

    updated exercises

--- JavaScript-Intro-Exercises.js
+++ JavaScript-Intro-Exercises.js
@@ -149,67 +149,84 @@
 // console.log(!!"0")
 // true
 
-let sample = "Hello Codeup";
+// let sample = "Hello Codeup";
+//
+// console.log(sample.length);
+//
+// console.log(sample.toLowerCase());
+//
+// let sampleJoin = `${sample} Students.`
+// console.log(sampleJoin);
 
-console.log(sample.length);
+// console.log(sample.indexOf("c"));
+//
+// console.log(sample.indexOf("C"));
+//
+// console.log(sample.indexOf(" "));
+//
+// console.log(sample.substring(6));
+//
+//
+// costPerDay = 3
+// let mermaid = costPerDay * 3;
+// let bear = costPerDay * 5;
+// let hercules = costPerDay * 1;
+// console.log(`The total cost is $${mermaid + bear + hercules}.`)
 
-console.log(sample.toLowerCase());
+// let googlePay = 400 * 6;
+// let amazonPay = 380 * 4;
+// let fbPay = 350 * 10;
+//
+// let total = googlePay + amazonPay + fbPay;
+// let formattedCheck = total.toLocaleString("en-US", {style:"currency", currency:"USD"});
+// console.log(formattedCheck);
 
-console.log(sample.indexOf("c"));
+//
+// let full = 25
+// let full = false
+// let freeTime = true
+// let available = !false
+//
+// let enrolled = true & true
+//
+//
+// let maxClassSize = 30
+// let scheduleConflicts = false
+// let currentClassSize = 24
+// let canEnroll = currentClassSize < maxClassSize && !scheduleConflicts;
+//
+//
 
-console.log(sample.indexOf("C"));
+let minCartSize = 2;
+let cartSize = 2;
+let vipMember = false;
+let offerExpired = false;
 
-console.log(sample.indexOf(" "));
+let canOfferApply = (vipMember || cartSize > minCartSize) && !offerExpired;
+console.log(canOfferApply);
 
-console.log(sample.substring(6));
-
-
-costPerday = 3
-let mermaid = costPerday * 3;
-let bear = costPerday * 5;
-let hercules = costPerday * 1;
-console.log( mermaid + bear + hercules);
-
-let googlePay = 400 * 6;
-let amazonPay = 380 * 4;
-let fbPay = 350 * 10;
-
-let total = googlePay + amazonPay + fbPay;
-console.log(total);
-
-let full = 25
-let full = false
-let freeTime = true
-let available = !false
-
-let enrolled = true & true
-
-
-let maxClassSize = 30
-let scheduleConflicts = false
-let currentClassSize = 24
-let canEnroll = currentClassSize < maxClassSize && !scheduleConflicts;
-
-
-let itemNumber
-let minimumItems = 3
-let offerexpired = false
-let vipMember = true
-let productOffer =
-
-
-var username = 'codeup';
-var password = 'notastrongpassword';
-
-let goodLength = 5
-let password.length = length
-let notLongEnough = length < goodLength = false
-
-let notOriginal = password.substring(0)
-
-
-
-
+// let username = "codeup";
+// let password = 'notastrongpassword';
+//
+//
+// let minChar = password.length >= 5;
+//
+// let noUsername = password.indexOf(username) === -1;
+// console.log(noUsername)
+//
+// let maxUserChar = username.length <=20;
+//
+// let userLength = username.length;
+// let passLength = passowrd.length;
+// let firstCharUser = username.substring(0,1);
+// let lastCharuser = username.substring((userLength - 1), userLength);
+// let firstCharPass = password.substring(0,1);
+// let lastCharPass = password.substring((substring((passLength - 1), passLength);
+// let hasWhiteSpace = firstCharUser === ' ' && lastCharUser === ' ' && firstCharPass === ' ' lastCharPass ===
+//
+//let hasWhitespace = firstCharUser === ' ' || lastCharUser === ' ' ||
+console.log(hasWhiteSpace);
+//
 
 
 
