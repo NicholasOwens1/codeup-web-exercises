154ce8282b8bfee529d0c1bc2bf0f4fe7f598b3a
Nicholas Owens <nicholas.s.owens01@gmail.com>
2023-03-13 16:39:37 -0500

    query exercise

---
+++ css/dom-lesson.js
@@ -0,0 +1,6 @@
+let myButton = document.querySelector('.btn')
+let allButtons = document.querySelectorAll('.btn')
+
+let clickMe = document.querySelector()
+console.log(clickMe.innerText);
+clickMe.innerText = "hello world";
