046fb5cd28bf667b1d167d8346b66ebb87b0d7d9
Nicholas Owens <nicholas.s.owens01@gmail.com>
2023-03-28 11:47:04 -0500

    working on exercises

---
+++ map-filter-reduce-lecture.js
@@ -0,0 +1,44 @@
+// const prices = [32.99, 21.99, 6.99, 4.99 , 12.99, 8.98, 5.99]
+
+// prices.forEach(price => {
+//     console.log (price + 1)
+// })
+
+// const pricesAfterTax = prices.map(price => {
+//     const tax = (price * 0.0825).toFixed(2);
+//     const total = (price + parseFloat(tax)).toFixed(2);
+//     return total;
+// })
+
+// let randomNumbers = [70, 30, 73, 22, 40, 48, 76, 20, 13, 92, 20, 56, 97, 76, 26, 77, 57, 47, 83, 70 ];
+//
+// const squared = randomNumbers.map(number => number ** 2);
+
+const cars = [
+    {
+        make: "Honda",
+        model: "Civic",
+        mileage: 10428
+    },
+    {
+        make: "Toyota",
+        model: "Corolla",
+        mileage: 9425
+    },
+    {
+        make: "Ford",
+        model: "Mustang",
+        mileage: 2567
+    },
+    {
+        make: "Audi",
+        model: "A3",
+        mileage: 14500
+    },
+    {
+        make: "Mazda",
+        model: "3",
+        mileage: 11248
+    }
+];
+
